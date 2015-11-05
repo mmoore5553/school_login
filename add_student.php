7a82ef2d24650782e6273b1f20b2fff18489ba04
Matthew Moore <mmoore5553@gmail.com>
2015-11-05 17:48:01 -0500

    added more to admin homepage and db
    I have added to admin homepage and actual write student or admin to
    database.

--- add_student.php
+++ add_student.php
@@ -5,24 +5,26 @@
     session_start();
     $role = $_SESSION['session_user_role'];
 
-   //  $sql_query = "INSERT INTO users (username,password,role) VALUES (:username,:password,:role)";
+    $sql_query = "INSERT INTO users (username,password,role) VALUES (:username,:password,:role)";
                                               
-   //  $sql = $database_connection->prepare($sql_query);
+    $sql = $database_connection->prepare($sql_query);
 
-   // // $data = array('username' => $_POST['username'] , 'password' => $_POST['password'], 'role' => $_POST['role']);
+   // $data = array('username' => $_POST['username'] , 'password' => $_POST['password'], 'role' => $_POST['role']);
 
 
                                           
-   //  $sql->bindParam(':username', $_POST['username'], PDO::PARAM_STR);       
-   //  $sql->bindParam(':password', $_POST['password'], PDO::PARAM_STR); 
-   //  $sql->bindParam(':role', $_POST['role'], PDO::PARAM_STR);
+    $sql->bindParam(':username', $_POST['username'], PDO::PARAM_STR);       
+    $sql->bindParam(':password', $_POST['password'], PDO::PARAM_STR); 
+    $sql->bindParam(':role', $_POST['role'], PDO::PARAM_STR);
+    
+    $data = array('username' => $_POST['username'] , 'password' => $_POST['password'], 'role' => $_POST['role']);
 
-   //  $data = array('username' => $_POST['username'] , 'password' => $_POST['password'], 'role' => $_POST['role']);
-
-   //  $sql->execute($data); 
+    $sql->execute($data); 
 
 
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -64,15 +66,24 @@
          <div class="col-md-3"></div>
             <div class="col-md-6 welcome-page">
               <h2>This is Admin area.</h2>
-
-              <div class="col-md-3">
-                
-                   <a class="btn btn-default" href="add_student.php">Add Student</a> 
-                    <li><a href="logout.php">Logout</a></li>
-                
-              </div>
-
-
+              
+              <div class ="enroll_div" >
+              <h2 class="label-info">Add Student</h2>
+                   <form class="" role="form" action="add_student.php" method="post">
+                      <div class="form-group">
+                        <label for="userName">User Name:</label>
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Enter Username">
+                      </div>
+                   <div class="form-group">
+                    <label for="Password">Password:</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password">
+                  </div>
+                  <div class="form-group">
+                    <label for="Role">Role: (Admin or User )</label>
+                    <input type="text" class="form-control" id="role" name="role" placeholder="Role is admin or user">
+                  </div>
+                  <button type="submit" class="btn btn-default">Submit</button>
+                 </form>
               </div>
 
               </div>
